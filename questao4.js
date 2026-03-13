2cf2bf88ff6d460e3119caacf73afc3845e49654
EmanuelDev <emanuel.silva06@aluno.ifce.edu.br>
2026-03-13 08:42:04 -0300

    Create questao4.js

---
+++ questao4.js
@@ -0,0 +1,20 @@
+function soma(a, b) {
+return a + b;
+}
+
+function multiplicar(a, b) {
+    return a * b;
+}
+
+function quadrado(a) {
+    return a ** a;
+}
+let resultado = soma(5, 3);
+let resultadoMult = multiplicar(5,3);
+let resultadoQuad = quadrado(5);
+console.log("Resultado da soma:", resultado);
+console.log("Resultado da multiplicação:", resultadoMult);
+console.log("Resultado do quadrado:", resultadoQuad);
+
+
+
