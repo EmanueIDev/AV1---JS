f981a74146acd94d765694059e2fe0549b1f7126
EmanuelDev <emanuel.silva06@aluno.ifce.edu.br>
2026-03-13 08:41:13 -0300

    Create questao1.js

---
+++ questao1.js
@@ -0,0 +1,8 @@
+let nome = "Noan";
+let idade = 16;
+let estudante = true;
+let curso = "Integrado em Informática";
+console.log("Nome:", nome);
+console.log("Idade:", idade);
+console.log("É estudante?", estudante);
+console.log(`${nome} tem ${idade} anos e cursa ${curso}`);
