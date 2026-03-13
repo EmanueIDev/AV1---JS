05176ae25a8d8fcc3f9c4991a212dcedba46a554
EmanuelDev <emanuel.silva06@aluno.ifce.edu.br>
2026-03-13 08:41:44 -0300

    Create questao3.js

---
+++ questao3.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+console.log("Número:", i);
+}
+
+console.log("Agora para mostrar números pares")
+for (let i = 0; i <= 20; i++) {
+    if(i % 2 == 0){
+console.log("Número:", i);
+}
+}
